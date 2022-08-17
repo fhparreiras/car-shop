37e0c8f966238c39d4e31e660796d1e6c81706fd
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-08-17 17:18:36 -0300

    feat: adiciona middleware de erro

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -33,7 +33,7 @@ abstract class MongoModel<T> implements IModel<T> {
   } 
 
   public async delete(_id:string):Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
     
     return this._model.findByIdAndRemove({ _id });
   }
