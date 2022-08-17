b1fed203e9246d570b5ca7d34134f449fd22b0bc
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-08-17 15:20:16 -0300

    test: adiciona teste da model Vehicle

--- src/tests/unit/models/vehicle.test.ts
+++ src/tests/unit/models/vehicle.test.ts
@@ -1,19 +1,30 @@
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import Vehicles from '../../../models/Vehicles';
+import { Model } from 'mongoose';
+import { vehicleMock, vehicleMockWithId } from '../../mocks/vehicleMock';
 
-// describe('Sua descrição', () => {
+describe('Vehicle Model', () => {
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  const vehicleModel = new Vehicles();
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  before(async () => {
+    sinon
+      .stub(Model, 'create').resolves(vehicleMockWithId);
+    sinon 
+      .stub(Model, 'findOne').resolves(vehicleMockWithId);
+  });
 
-//   it('', async () => {});
+  after(()=>{
+    sinon.restore();
+  })
 
-// });
+  describe('Criando um veículo', () => {
+    it('é criado com sucesso', async () => {
+      const newVehicle = await vehicleModel.create(vehicleMock);
+      expect(newVehicle).to.be.deep.equal(vehicleMockWithId);
+    });
+  })
+
+});
