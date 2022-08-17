30fa1f301f01d8cd445e4a9580d8188d54a16ccf
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-08-17 20:33:22 -0300

    fix: erro lint

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import IService from '../interfaces/IService';
 import { ICar } from '../interfaces/ICar';
-import { ErrorTypes } from '../errors/catalog';
 
 export default class CarController {
   constructor(private _service: IService<ICar>) { }
