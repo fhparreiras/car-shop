217e89903f283a7d8f870dd3ad53d7dda158e90f
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-08-17 20:03:02 -0300

    feat: adiciona os métodos de read, update e delete no controller

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -23,11 +23,36 @@ export default class CarController {
     return res.status(201).json(results);
   }
 
+  public async read(
+    req: Request,
+    res: Response<ICar[]>,
+  ) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
   public async readOne(
     req: Request,
     res: Response<ICar | null>,
   ) {
     const result = await this._service.readOne(req.params.id);
     return res.status(200).json(result);
+  }
+
+  public async update(
+    req: Request, 
+    res: Response<ICar | null>,
+  ) {
+    const result = await this._service.update(req.params.id, req.body);
+    return res.status(201).json(result);
+  }
+
+  public async delete(
+   
+    req: Request, 
+    res: Response<ICar | null>,
+  ) {
+    const result = await this._service.delete(req.params.id);
+    return res.status(201).json(result);
   }
 }
