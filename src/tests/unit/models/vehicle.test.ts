253830645a2bc38468d7733d8352501974ebb25b
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-08-17 19:37:49 -0300

    test: aumenta testes da model

--- src/tests/unit/models/vehicle.test.ts
+++ src/tests/unit/models/vehicle.test.ts
@@ -30,6 +30,13 @@ describe('Cars Model', () => {
     });
   })
 
+  describe('Procurando todos os veículos', () => {
+		it('encontra todos os veículos com sucesso', async () => {
+			const vehicleFound = await vehicleModel.read();
+			expect(vehicleFound).to.be.deep.equal(allVehicleMock);
+		});
+	});
+
   describe('Procurando um veículo', () => {
 		it('encontrado com sucesso', async () => {
 			const vehicleFound = await vehicleModel.readOne('62cf1fc6498565d94eba52cd');
@@ -38,11 +45,40 @@ describe('Cars Model', () => {
 
 		it('_id não encontrado', async () => {
 			try {
-				await vehicleModel.readOne('123ERRADO');
+				await vehicleModel.readOne('23xablau312');
 			} catch (error: any) {
 				expect(error.message).to.be.eq('InvalidMongoId');
 			}
 		});
 	});
 
+  describe('Atualizando um veículo', () => {
+		it('atualizado com sucesso', async () => {
+			const vehiclesChanged = await vehicleModel.update('62cf1fc6498565d94eba52cd', vehicleMockForChange);
+			expect(vehiclesChanged).to.be.deep.equal(vehicleMockForChange);
+		});
+
+		it('_id não encontrado para poder atualizar', async () => {
+			try {
+				await vehicleModel.update('23xablau312', vehicleMockForChange);
+			} catch (error: any) {
+				expect(error.message).to.be.eq('InvalidMongoId');
+			}
+		});
+	});
+
+  describe('Deletando um veículo', () => {
+		it('deletado com sucesso', async () => {
+			const vehiclesChanged = await vehicleModel.delete('62cf1fc6498565d94eba52cd');
+			expect(vehiclesChanged).to.be.deep.equal(vehicleMockForChange);
+		});
+
+		it('_id not found to remove', async () => {
+			try {
+				await vehicleModel.delete('123xablau312');
+			} catch (error: any) {
+				expect(error.message).to.be.eq('InvalidMongoId');
+			}
+		});
+	});
 });
